f34153b84d5cca6a6b7048957a3359491d734ac5
1hiteshk <1hiteshk@gmail.com>
2024-07-31 14:30:15 +0530

    update : removed unused code & updates

--- frontend/src/components/cards/ProjectCard.tsx
+++ frontend/src/components/cards/ProjectCard.tsx
@@ -36,46 +36,6 @@ interface ProjectCardProps {
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ data }) => {
   const { _id, projectNumber, projectTitle, createdAt, updatedAt } = data;
-  const dispatch = useDispatch();
-  const toast = useToast();
-
-  const handleDelete = async () => {
-    try {
-      await api.delete(`/projects/${_id}`); // Make API call to delete project
-      dispatch(removeUserProject(_id)); // Dispatch action to update the Redux state
-      dispatch(deleteTasksByProjectId(_id));
-      toast({
-        duration: 5000,
-        position: "bottom-right",
-        isClosable: true,
-        render: ({ onClose }) => (
-          <CustomToast
-            duration={5000}
-            title={`Project ${projectNumber} deleted.`}
-            description={`The project ${projectTitle} and its associated tasks have been deleted.`}
-            status="success"
-            onClose={onClose}
-          />
-        ),
-      });
-    } catch (error) {
-      console.error("Error deleting project:", error);
-      toast({
-        duration: 5000,
-        isClosable: true,
-        position: "bottom-right",
-        render: ({ onClose }) => (
-          <CustomToast
-            duration={5000}
-            title={`Error deleting project ${projectNumber}.`}
-            description="There was an error deleting the project."
-            status="error"
-            onClose={onClose}
-          />
-        ),
-      });
-    }
-  };
 
   return (
     <Card
